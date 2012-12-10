0a3cc29e21caa368b620e63fa235d839a0afa63a
Gin-san <gin.san.rzlk.g@gmail.com>
2012-12-10 09:33:31 +0100

    can define user agent in curl execute method

--- src/Tools/Curl/Curl.php
+++ src/Tools/Curl/Curl.php
@@ -5,12 +5,16 @@
 
 namespace Tools\Curl;
 
-use Tools\Curl\UrlGenerator;
+use Tools\Curl\UrlGenerator,
+  Tools\Curl\Constants;
 
 class Curl
 {
-
-  //public static function execute(string $url, array $parameters, string $method = "GET", array $tags = array())
+  /*
+  ** execute a http request and return the server response
+  ** manager GET/POST
+  ** manager url with tags like '{name}' or parameters for get or post request
+  */
   public static function execute($url, $parameters, $method = "GET", $tags = array())
   {
     if (is_array($tags) && count($tags) > 1) {
@@ -18,11 +22,13 @@ class Curl
     }
 
     $url = strtolower($method) == "get" ? UrlGenerator::generateGetUrl($url, $parameters) : $url;
-    //echo $url . "\n";
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true); 
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+    if (defined("CURL_USER_AGENT")) {
+      curl_setopt($ch,CURLOPT_USERAGENT, constant("CURL_USER_AGENT"));
+    }
     if (strtolower($url) == "post") {
       curl_setopt($ch, CURLOPT_POST, true);
       if (is_array($parameters) && count($parameters) > 1) {
@@ -32,11 +38,14 @@ class Curl
 
     $response = curl_exec($ch);
     $info = curl_getinfo($ch);
-    //print_r($info);
     curl_close($ch);
     return $response;
   }
 
+  /*
+  ** Download an image from a url with the filename given
+  ** return true if file uploaded or false otherwise
+  */
   public static function fetchimage(string $url_img, string $filename)
   {
     if (file_exists($filename)) {
