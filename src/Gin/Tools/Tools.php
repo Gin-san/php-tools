95e5e94b56765f8e31710012b0f35a61c406b0e5
Gin-san <gin.san.rzlk.g@gmail.com>
2013-02-17 15:55:09 +0100

    add possibility to clear a text

--- src/Gin/Tools/Tools.php
+++ src/Gin/Tools/Tools.php
@@ -17,4 +17,10 @@ class Tools
     }
     return round($mem_usage / 1048576, 2) . " megabytes";
   }
+
+  public static function clearUTF($text)
+  {
+    setlocale(LC_ALL, 'en_US.UTF8');
+    return str_replace(' ', '_', trim(preg_replace('#[^\p{L}\p{N}]+#u', ' ', strtolower(iconv('UTF-8', 'ASCII//TRANSLIT', $text)))));
+  }
 }
