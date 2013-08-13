4dabbc3abd0ba7f070b78a985eb0fac00b5457df
Gin-san <gin.san.rzlk.g@gmail.com>
2013-08-13 14:39:40 +0200

    fix on curl tools and add possibility to use cookies and http_header options

--- src/Gin/Tools/Curl/Curl.php
+++ src/Gin/Tools/Curl/Curl.php
@@ -9,14 +9,21 @@ use Gin\Tools\Curl\UrlGenerator;
 
 class Curl
 {
+  static $info = null;
+
+  public static function getInfo()
+  {
+    return self::$info;
+  }
+
   /*
   ** execute a http request and return the server response
   ** manager GET/POST
   ** manager url with tags like '{name}' or parameters for get or post request
   */
-  public static function execute($url, $parameters, $method = "GET", $tags = array())
+  public static function execute($url, $parameters, $method = "GET", $tags = array(), $options = array())
   {
-    if (is_array($tags) && count($tags) > 1) {
+    if (is_array($tags) && count($tags) > 0) {
       $url = UrlGenerator::generateTaggedUrl($url, $tags);
     }
 
@@ -25,18 +32,30 @@ class Curl
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+    if (defined("CURL_COOKIE_PATH")) {
+      curl_setopt( $ch, CURLOPT_COOKIESESSION, true );
+      curl_setopt( $ch, CURLOPT_COOKIEJAR, constant("CURL_COOKIE_PATH"));
+      curl_setopt( $ch, CURLOPT_COOKIEFILE, constant("CURL_COOKIE_PATH"));
+    }
+
     if (defined("CURL_USER_AGENT")) {
       curl_setopt($ch,CURLOPT_USERAGENT, constant("CURL_USER_AGENT"));
     }
-    if (strtolower($url) == "post") {
+
+    if (strtolower($methode) == "post") {
       curl_setopt($ch, CURLOPT_POST, true);
-      if (is_array($parameters) && count($parameters) > 1) {
-	curl_setopt($ch, CURLOPT_POSTFIELDS, $parameters);
+      if (is_array($parameters) && count($parameters) > 0) {
+	curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($parameters));
       }
     }
 
+    if (isset($options['http_header']) && is_array($options['http_header'])) {
+      curl_setopt($ch, CURLOPT_HTTPHEADER, $options['http_header']);
+    }
+
+
     $response = curl_exec($ch);
-    $info = curl_getinfo($ch);
+    self::$info = curl_getinfo($ch);
     curl_close($ch);
     return $response;
   }
